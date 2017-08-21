96657badce593d65353b1d91cf771c49d3e00bb6
melganainy <melganainy@smartera3s.com>
2017-08-21 18:53:52 +0200

    Add legend to Plot3

--- plot3.R
+++ plot3.R
@@ -12,4 +12,9 @@ plot(time_col, pwrd_2d$Sub_metering_1, type='l', ylab="Energy sub metering", xla
 lines(time_col, pwrd_2d$Sub_metering_2, type='l', col="red")
 lines(time_col, pwrd_2d$Sub_metering_3, type='l', col="blue")
 
+legend("topright", 
+       c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+       lty=c(1,1,1),
+       col=c("black", "red", "blue"))
+
 dev.off()
